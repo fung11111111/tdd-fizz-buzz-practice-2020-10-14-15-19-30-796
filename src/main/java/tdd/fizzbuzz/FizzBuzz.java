edcfa7d0c34110f24c13717ca12599fa7d05a6b9
Ray Lam <ray.lam@oocl.com>
2020-11-26 13:58:27 +0800

    edit FizzBuzzPass

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,10 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public String countOFF(int order) {
 
+        if(order % 3 == 0){
+            return "Fizz";
+        }
+
         return String.valueOf(order);
     }
 }
