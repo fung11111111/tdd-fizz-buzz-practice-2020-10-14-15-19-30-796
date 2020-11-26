9a4bc328f9c5044f02ed3e26368a5384683b8ff0
Ray Lam <ray.lam@oocl.com>
2020-11-26 14:03:56 +0800

    add BuzzLogic

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,10 +4,13 @@ public class FizzBuzz {
     public static final String FIZZ = "Fizz";
     public static final int MODULO_3 = 3;
 
+
     public String countOFF(int order) {
 
         if(order % MODULO_3 == 0){
             return FIZZ;
+        }else if(order % 5 ==0){
+            return "Buzz";
         }
 
         return String.valueOf(order);
