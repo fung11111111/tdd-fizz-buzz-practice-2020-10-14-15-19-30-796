5483f05a62b4271bb37e659c33bf710d96bc606e
Ray Lam <ray.lam@oocl.com>
2020-11-26 14:05:53 +0800

    code refractoring - add constant

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,15 +2,18 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
     public static final int MODULO_3 = 3;
+    public static final int MODULO_5 = 5;
+
 
 
     public String countOFF(int order) {
 
         if(order % MODULO_3 == 0){
             return FIZZ;
-        }else if(order % 5 ==0){
-            return "Buzz";
+        }else if(order % MODULO_5 == 0){
+            return BUZZ;
         }
 
         return String.valueOf(order);
