cc5b4e2135f5d66042493dc800660d3ff6d2e02a
Ray Lam <ray.lam@oocl.com>
2020-11-26 14:15:55 +0800

    Update FizzBuzzTest.java
    add should_return_FizzBuzzWhizz_when_countOff_given_order_num_105Fail

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -65,6 +65,17 @@ public class FizzBuzzTest {
         //then
         assertEquals("FizzBuzz", actual);
     }
+    @Test
+    void should_return_FizzBuzzWhizz_when_countOff_given_order_num_105(){
+        //give
+        int order = 105;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // when
+        String actual = fizzBuzz.countOFF(order);
+
+        //then
+        assertEquals("FizzBuzzWhizz", actual);
+    }
 
 
 
